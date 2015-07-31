73920fc9cb255e196cd18e67ab321851d4a7462d
David Larson <dplarson@ucsd.edu>
2015-07-30 23:37:21 -0700

    Revisions for PrEP workshop (2015)
    Revise README and sample code for use in a programming workshop for the
    2015 Summer PrEP program.

--- README.md
+++ README.md
@@ -3,3 +3,22 @@ Files for learning Matlab.
 
 ## Requirements
 - Matlab
+
+## Topics covered
+- basic math operations
+- variables
+- math functions
+- loops
+- plotting
+
+--------------------------------------------------
+
+## Online demos
+Although Matlab is very useful, let's also look at a few other demos for things you can accomplish using programming:
+- [Cat detector](https://harthur.github.io/kittydar/)
+- [How old do I Look?](https://how-old.net/)
+- [Park or Bird?](http://parkorbird.flickr.com/) (inspired by [XKCD #1425](https://xkcd.com/1425/))
+- [Image classification](http://demo.caffe.berkeleyvision.org/)
+- [Math visualization](https://maxbittker.github.io/Mojulo/)
+- [Interactive visuals](http://www.redblobgames.com/)
+- [Fluid Dynamics simulation](https://nerget.com/fluidSim/)
--- intro_to_matlab.m
+++ intro_to_matlab.m
@@ -5,7 +5,7 @@
 % IDEA Center, UC San Diego
 
 
-%% Basic math operations
+%% 1. Basic math operations
 %
 % + = addition
 % - = subtraction
@@ -16,59 +16,99 @@
 
 2 + 2
 
-% NOTE: you can suppress output using a semi-colon
-2 + 2;
+% NOTE: anything with a percent sign in front (like this line) is a comment
+% and will not be executed. Remove the percent sign to "uncomment" the code
+% and allow it to be run.
+%3 * 3
 
-%% Variables
+
+% NOTE: you can suppress output using a semi-colon
+%2 + 2;
+
+
+%% 2. Variables
 % 
 % Variables can be assigned (like in any other programming language) using
 % an equal sign
 %
 
-x = 5;
+%x = 5;
+
+% example: create a variable with value 9.81 (the acceleration due to
+% gravity, in m/s^2)
 
 
-%% Vectors
+% NOTE: Matlab has some built-in variables (e.g. pi)
+%pi
+
+% example: calculate the value of "two pi"
+
+
+
+%% 3. Math functions
 %
-% Creating vectors is fairly straight-forward.
-%
-
-% variable = [start:step size:end]
-a = [0:1:5];
-
-% can also create vectors with a specific number of elements using linspace
-% variable = linspace(start, end, number of elements)
-
-
-%% Vector operations
-%
-% To perform a vector operation (e.g. multiply the elements of two
-% vectors), place a period in front of a math operation.
+% Matlab has a lot of built-in functions for common math operations (e.g.
+% cosine)
 %
 
-% create two vectors of equal length
-a = [0:1:5];
-b = [0:1:5];
+% the function "cos" calculates the cosine value of a given angle in
+% radians (not degrees)
+%cos(pi)
 
-% multiply the two vectors element-by-element
-c = a .* b;
+% notice that the order is: function name, open parenthesis, input, closed
+% parenthesis
 
-% you can also add, subtract, etc.
+% example: calculate the sine of 0
 
 
-%% Plotting
+
+%% 4. Loops
+%
+% For-loops are a simple way to iterate through a range of numbers,
+% variables, etc. (instead of writing the same code over and over)
+%
+
+% this display the value for i, starting with i=1 and ending with i=3
+%for i=1:3
+%    i
+%end
+
+% what does this do?
+%for x=1:3:9
+%   x
+%end
+
+% example: try to calculate the square of the even numbers from 2 through 6
+% (remember, you can square a number with "^", e.g. "3^2" is three squared)
+
+
+
+%% 5. Plotting
 %
 % Matlab has built-in plotting functionality and includes most common
 % plotting methods (e.g. scatter plot, box plot, etc.).
 %
 
-% plot two vectors
-x = [0:0.1:10];
-y = cos(x);
-plot(x, y)
+% this code creates two variables: x and y
+% - x is a list of numbers from 0 to 6, in steps of 0.1
+% - y is the cosine value of the numbers in x
 
-% you can modify the plots manually using Tools >> Edit Plot or you can
-% specify your revisions in the code. Example:
+%x = [0:0.1:6];
+%y = cos(x);
+
+% to plot x vs y, just use the "plot" function
+%plot(x, y)
+
+% NOTE: you can modify the plots manually using Tools >> Edit Plot or you
+% can specify your revisions in the code. Example:
 %
 % set the x-axis label
 %xlabel('x')
+
+% try adding a label to the y-axis
+
+
+% now create another variable "z" that is the sine value of the numbers in
+% the vector "x"
+
+
